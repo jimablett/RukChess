4a8e9b71e73137cfb6ed5818b68cb1a8a9b95d2f
Ilya Rukavishnikov <ruk@mgpu.ru>
2023-12-30 21:43:02 +0300

    3.0.19dev

--- Def.h
+++ Def.h
@@ -168,7 +168,7 @@
 // Program name, program version, algorithm name, evaluation function name and copyright information
 
 #define PROGRAM_NAME                            "RukChess"
-#define PROGRAM_VERSION                         "3.0.18"
+#define PROGRAM_VERSION                         "3.0.19dev"
 
 #ifdef MCTS
 #define ALGORITHM_NAME                          "MCTS"
