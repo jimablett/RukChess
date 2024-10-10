aa3a14e16c1dd9ca57e03014f2a6e3327353b37d
Илья Рукавишников <ruk@mgpu.ru>
2024-10-10 12:26:55 +0300

    Corrected the code

--- Utils.cpp
+++ Utils.cpp
@@ -74,7 +74,12 @@ void InitThreadNode(void)
     Buffer = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX*)malloc(ReturnedLength);
 
     if (Buffer == NULL) { // Allocate memory error
-        printf("Allocate memory to buffer error!\n");
+        if (PrintMode == PRINT_MODE_NORMAL) {
+            printf("Allocate memory to buffer error!\n");
+        }
+        else if (PrintMode == PRINT_MODE_UCI) {
+            printf("info string Allocate memory to buffer error!\n");
+        }
 
         return;
     }
